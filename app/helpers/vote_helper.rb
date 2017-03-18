e7841480d13d4e5205762f86a49f50ad3deacf84
Jesse Calton & Joseph Huang <pair+jessecalton.timeswap@devbootcamp.com>
2017-03-17 17:34:18 -0700

    have working upvotes and downvote methods - one per user

--- app/helpers/vote_helper.rb
+++ app/helpers/vote_helper.rb
@@ -1,35 +1,54 @@
 helpers do
   def upvote_answer
     @answer = Answer.find(params[:id])
-    @answer.votes.create(value: "updoot", user_id: @answer.user_id)
+    @vote = @answer.votes.find_by(user_id: current_user.id)
+    if @vote
+      @vote.update_attributes(value: "updoot")
+    else
+      Vote.create(value: "updoot", user_id: current_user.id, votable_id: @answer.id, votable_type: "Answer")
+    end
   end
 
   def upvote_question
     @question = Question.find(params[:id])
-    @question.votes.find_or_create_by(value: "updoot", user_id: @question.user_id)
+    @vote = @question.votes.find_by(user_id: current_user.id)
+    if @vote
+      @vote.update_attributes(value: "updoot")
+    else
+      Vote.create(value: "updoot", user_id: current_user.id, votable_id: @question.id, votable_type: "Question")
+    end
   end
-# test
+
   def downvote_question
     @question = Question.find(params[:id])
-    @question.votes.find_or_create_by(value: "downdoot", user_id: @question.user_id)
+    @vote = @question.votes.find_by(user_id: current_user.id)
+    if @vote
+      @vote.update_attributes(value: "downdoot")
+    else
+      Vote.create(value: "downdoot", user_id: current_user.id, votable_id: @question.id, votable_type: "Question")
+    end
   end
 
   def downvote_answer
     @answer = Answer.find(params[:id])
-    @answer.votes.find_by(user_id: params[:id]).delete
+    @vote = @answer.votes.find_by(user_id: current_user.id)
+    if @vote
+      @vote.update_attributes(value: "downdoot")
+    else
+      Vote.create(value: "downdoot", user_id: current_user.id, votable_id: @answer.id, votable_type: "Answer")
+    end
   end
 
   def count_votes(args)
-    updoots = 0
-    downdoots = 0
+    counter = 0
     args.votes.each do |vote|
       if vote.value == "updoot"
-        updoots += 1
+        counter += 1
       elsif vote.value == "downdoot"
-        updoots -= 1
+        counter -= 1
       end
     end
-    return updoots
+    return counter
   end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,7 @@
 class Vote < ActiveRecord::Base
   validates_uniqueness_of :user_id, scope: [:votable_type, :votable_id]
+  belongs_to :questions
+  belongs_to :comments
+  belongs_to :answers
+
 end
